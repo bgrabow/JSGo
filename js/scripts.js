3db0d37ea7a409187a910392ababf1cf2578f351
bgrabow <ben.grabow@gmail.com>
2016-09-30 17:49:27 -0500

    Render stones.
    We currently take the naive approach of visiting each UI cell, removing the current stone, and inserting a new stone if necessary.

--- js/scripts.js
+++ js/scripts.js
@@ -20,27 +20,49 @@ class GoGame {
 }
 
 class State {
-    constructor(inCells = new Map()) {
+    constructor(inCells = new StoneMap()) {
         var cells = inCells;
         this.currentPlayerColor = Stone.black;
 
         this.stoneAt = (col, row) => {
-            return cells.has([col, row]) ?
-                   cells.get([col, row]) :
+            return cells.has(col, row) ?
+                   cells.get(col, row) :
                    Stone.empty;
         }
 
         this.add = (stone, col, row) => {
-            var newCells = new Map(cells);
-            newCells.set([col, row], stone);
+            var newCells = new StoneMap(cells);
+            newCells.set(col, row, stone);
             return new State(newCells);
         }
 
         this.getValue = () => {
             return {
                 currentPlayerColor: this.currentPlayerColor,
-                cells: new Map(cells),
+                cells: new StoneMap(cells),
             }
         }
+    }
+}
+
+class StoneMap {
+    constructor(stoneMap) {
+        this.map = stoneMap ? new Map(stoneMap.map) : new Map();
+    }
+
+    has(col, row) {
+        return this.map.has(this.key(col, row));
+    }
+
+    get(col, row) {
+        return this.map.get(this.key(col, row));
+    }
+
+    set(col, row, value) {
+        return this.map.set(this.key(col, row), value);
+    }
+
+    key(col, row) {
+        return [col, row].toString();
     }
 }
