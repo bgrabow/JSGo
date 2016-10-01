95e71b04397380d865fc480c31748939f0695330
bgrabow <ben.grabow@gmail.com>
2016-10-01 18:39:34 -0500

    Foramatted some tests

--- test/mySpec.js
+++ test/mySpec.js
@@ -6,34 +6,36 @@ describe("A suite", () => {
 
 describe("The game of Go", () => {
     describe("Playing a stone", () => {
-        var game;
-        const [SOME_COL, SOME_ROW] = [0, 0];
-        const [OTHER_COL, OTHER_ROW] = [1, 1];
+        describe("A basic play", ()=>{
+            var game;
+            const [SOME_COL, SOME_ROW] = [0, 0];
+            const [OTHER_COL, OTHER_ROW] = [1, 1];
 
-        beforeEach(() => {
-            game = new GoGame();
-        })
+            beforeEach(() => {
+                game = new GoGame();
+            })
 
-        it("adds a stone to the board", ()=>{
-            game.currentPlayerSelects(SOME_COL, SOME_ROW);
-            expect(game.state.stoneAt(SOME_COL, SOME_ROW)).toBe(Stone.black);
-        })
+            it("adds a stone to the board", ()=>{
+                game.currentPlayerSelects(SOME_COL, SOME_ROW);
+                expect(game.state.stoneAt(SOME_COL, SOME_ROW)).toBe(Stone.black);
+            })
 
-        it("can't be done on an occupied cell", ()=>{
-            game.currentPlayerSelects(SOME_COL, SOME_ROW);
-            let stonesAfterFirstPlay = game.state.cells.size();
-            game.currentPlayerSelects(SOME_COL, SOME_ROW);
-            expect(game.state.cells.size()).toBe(stonesAfterFirstPlay);
-        })
+            it("can't be done on an occupied cell", ()=>{
+                game.currentPlayerSelects(SOME_COL, SOME_ROW);
+                let stonesAfterFirstPlay = game.state.cells.size();
+                game.currentPlayerSelects(SOME_COL, SOME_ROW);
+                expect(game.state.cells.size()).toBe(stonesAfterFirstPlay);
+            })
 
-        it("changes the current player", ()=>{
-            let firstPlayer = game.currentPlayer();
+            it("changes the current player", ()=>{
+                let firstPlayer = game.currentPlayer();
 
-            game.currentPlayerSelects(SOME_COL, SOME_ROW);
-            expect(game.currentPlayer() != firstPlayer).toBe(true);
+                game.currentPlayerSelects(SOME_COL, SOME_ROW);
+                expect(game.currentPlayer() != firstPlayer).toBe(true);
 
-            game.currentPlayerSelects(OTHER_COL, OTHER_ROW);
-            expect(game.currentPlayer()).toBe(firstPlayer);
+                game.currentPlayerSelects(OTHER_COL, OTHER_ROW);
+                expect(game.currentPlayer()).toBe(firstPlayer);
+            })
         })
     })
 })
